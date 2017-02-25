ee7459dabafd60ab904b45e479752e850e8d0009
Peter <krutsiuk.petr@gmail.com>
2017-02-25 20:20:22 +0200

    Add custom scope

--- src/main/java/com/krupet/screensaver/Config.java
+++ src/main/java/com/krupet/screensaver/Config.java
@@ -15,7 +15,7 @@ import java.util.Random;
 public class Config {
     @Bean
 //    @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
-    @Scope("prototype")
+    @Scope("periodical")
     public Color color() {
         Random random = new Random();
         return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
@@ -35,7 +35,7 @@ public class Config {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
         while (true) {
             context.getBean(ColorFrame.class).showOnRandomPlace();
-            Thread.sleep(50);
+            Thread.sleep(150);
         }
     }
 }
